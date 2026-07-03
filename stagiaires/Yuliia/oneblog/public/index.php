21d49ccd732e3a299d3c26bf6b363e9afb4c69bd
julia-dm <yulia.2508dmitruk@gmail.com>
2026-07-03 12:18:55 +0200

    MVC connection PDO

---
+++ stagiaires/Yuliia/oneblog/public/index.php
@@ -0,0 +1,18 @@
+<?php
+#stagiaires/Yuliia/oneblog/public/index.php
+
+/**
+ * Controleur frontal, seul fichier accecible du site
+ */
+
+/**
+ * Appel des dependances
+*/
+
+// fichier de configuration
+require_once "../config.php";
+
+/**
+ * Chargement de routeur
+ */
+require_once BASE_URL."/controller/routercontroller.php";
