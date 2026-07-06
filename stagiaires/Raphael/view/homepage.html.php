0a9a9575d39d486dc7a8f6d7734560a7ca896edb
raphaeldebot <debot.raphael@gmail.com>
2026-07-06 12:07:28 +0200

    explode categ

--- stagiaires/Raphael/view/homepage.html.php
+++ stagiaires/Raphael/view/homepage.html.php
@@ -12,39 +12,72 @@
         <link href="css/styles.css" rel="stylesheet" />
     </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <a class="navbar-brand" href="#">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Link</a></li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><hr class="dropdown-divider" /></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    <?php include_once BASE_URL."/view/inc/menu.public.html.php"?>
     <!-- Page content-->
         <div class="container">
             <div class="text-center mt-5">
                 <h1>OneBlog | Accueil</h1>
                 <p class="lead">Page d'accueil de mon blog</p>
-                
             </div>
+
+            <?php
+
+            if(empty($articles)):
+            ?>
+            <h3>Pas encore d'arcticle</h3>
+            <?php
+            else:
+                // on va compter le nombre d'articles
+                $count = count($articles);
+                $pluriel = ($count>1)? "s" : "";
+            ?>
+            <h4>Il y a <?=$count?> article<?=$pluriel?></h4>
+                <!-- Three columns of text below the carousel -->
+    <div class="row">
+            <?php
+            // tant qu'on a des articles
+            foreach($articles as $article):
+            ?>
+            
+
+      <div class="col-lg-4">
+        <title>Placeholder</title>
+        <h3><a href="?idarticle=<?p= $article['id']?>"><?= $article['title'] ?></a></h3>
+        <?php
+        if(is_null($article['idcategory'])):
+        ?>
+        <h5>aucune catégorie</h5>
+        <?php
+
+        else:
+                $idcateg = explode(",", $article['idcategory']);
+                $titlecategory = explode("_|♥|_", $article['titlecategory']);
+                $nbcateg = count($idcateg);
+        ?>
+        <h5><?php
+            for($i=0;$i<$nbcateg; $i++):
+                ?>
+            <a href="?idcateg=<?= $idcateg[$i]?>"><?= $titlecategory[$i]?></a>"_|♥|_"
+                <?php
+            endfor;
+        ?></h5>
+
+        <?php
+        endif;
+        ?>
+        <p class="lead">écrit pas <a href="?iduser=<?= $article["iduser"] ?>"><?= $article['realname'] ?></a> le <?= $article['datetime'] ?> </p>
+        <p>Notre texte résumé</p>
+      </div>
+            <?php
+                endforeach;
+            endif;
+            ?>
         </div>
         <!-- Bootstrap core JS-->
-        <script src="js/boostrap.bundle.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
-    <?php var_dump($connection,$menu)?>
+        
+    <?php var_dump($connection,$menu,$articles)?>
 </body>
 </html>
